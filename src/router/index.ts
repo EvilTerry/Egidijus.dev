dce25e792b74c1ed0bed67b045279952ed1b9380
Egidijus <evilterixyoutub@gmail.com>
2026-04-20 18:36:51 +0200

    refactor: update router configuration and add NotFound view

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,27 +1,23 @@
 import { createRouter, createWebHistory } from "vue-router";
+import type { RouteRecordRaw } from "vue-router";
+
 import HomeView from "@/views/HomeView.vue";
 import AboutView from "@/views/AboutView.vue";
 import ProjectsView from "@/views/ProjectsView.vue";
+import ProjectView from "@/views/ProjectView.vue";
+import NotFound from "@/views/NotFound.vue";
+
+const routes: RouteRecordRaw[] = [
+  { path: "/", component: HomeView },
+  { path: "/about", component: AboutView },
+  { path: "/projects", component: ProjectsView },
+  { path: "/projects:slug", component: ProjectView },
+  { path: "/:pathMatch(.*)*", component: NotFound },
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: "/",
-      name: "home",
-      component: HomeView,
-    },
-    {
-      path: "/about",
-      name: "about",
-      component: AboutView,
-    },
-    {
-      path: "/projects",
-      name: "projects",
-      component: ProjectsView,
-    },
-  ],
+  routes: routes,
 });
 
 export default router;
